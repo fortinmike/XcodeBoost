60d73095118dfbf22911af22edb5746864476bfd
Michaël Fortin <fortinmike@irradiated.net>
2014-03-19 16:51:20 -0400

    Registering for active editor change notifications and creating a submenu for the Text Tools menu item.

--- XcodeTextTools/MFPluginController.m
+++ XcodeTextTools/MFPluginController.m
@@ -31,19 +31,33 @@
 
 - (void)insertMenuItems
 {
-	NSMenuItem *textToolsMenuItem = [[NSMenuItem alloc] initWithTitle:@"Text Tools" action:NULL keyEquivalent:@""];
-	NSArray *menuItems = @[[NSMenuItem separatorItem], textToolsMenuItem];
+	NSArray *menuItems = @[[NSMenuItem separatorItem], [self createTextToolsMenuItem]];
 	
 	NSMenu *editMenu = [[[NSApp mainMenu] itemWithTitle:@"Edit"] submenu];
 	[editMenu xctt_insertItems:menuItems beforeItem:1 where:^BOOL(NSMenuItem *item) { return [item isSeparatorItem]; }];
 }
 
+- (NSMenuItem *)createTextToolsMenuItem
+{
+	NSMenu *submenu = [[NSMenu alloc] init];
+	[submenu addItem:[[NSMenuItem alloc] initWithTitle:@"Cut Line" action:nil keyEquivalent:@""]];
+	[submenu addItem:[[NSMenuItem alloc] initWithTitle:@"Copy Line" action:nil keyEquivalent:@""]];
+	[submenu addItem:[[NSMenuItem alloc] initWithTitle:@"Paste Line" action:nil keyEquivalent:@""]];
+	[submenu addItem:[[NSMenuItem alloc] initWithTitle:@"Duplicate Line" action:nil keyEquivalent:@""]];
+	[submenu addItem:[[NSMenuItem alloc] initWithTitle:@"Delete Line" action:nil keyEquivalent:@""]];
+	
+	NSMenuItem *textToolsMenuItem = [[NSMenuItem alloc] initWithTitle:@"Text Tools" action:NULL keyEquivalent:@""];
+	[textToolsMenuItem setSubmenu:submenu];
+	
+	return textToolsMenuItem;
+}
+
 - (void)registerForNotifications
 {
-//	[[NSNotificationCenter defaultCenter] addObserver:self
-//											 selector:@selector(activeEditorContextDidChange:)
-//												 name:@"IDEEditorAreaLastActiveEditorContextDidChangeNotification"
-//											   object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(activeEditorContextDidChange:)
+												 name:@"IDEEditorAreaLastActiveEditorContextDidChangeNotification"
+											   object:nil];
 }
 
 - (void)dealloc
@@ -55,7 +69,23 @@
 
 - (void)activeEditorContextDidChange:(NSNotification *)notification
 {
-	[[NSAlert alertWithMessageText:@"Change!" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""] runModal];
+	NSLog(@"Test");
+	
+//	IDEEditorContext *context = [notification userInfo][@"IDEEditorContext"];
+//    self.activeEditor = [self getSourceTextViewFromEditorContext:context];
 }
+
+//- (DVTSourceTextView *)getSourceTextViewFromEditorContext:(IDEEditorContext *)context
+//{
+//    IDEEditor *editor = [context editor];
+//    NSScrollView *scrollView = [editor mainScrollView];
+//    NSClipView *clipView = [scrollView contentView];
+//    id documentView = [clipView documentView];
+//    if ([documentView isKindOfClass:[DVTSourceTextView class]]) {
+//        return documentView;
+//    } else {
+//        return nil;
+//    }
+//}
 
 @end
