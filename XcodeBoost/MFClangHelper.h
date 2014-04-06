757a176a6d5adca7d53cbb61056d7f22676b3f22
Michaël Fortin <fortinmike@irradiated.net>
2014-04-06 17:12:31 -0400

    Added Clang header files and header search path, MFClangHelper class.

---
+++ XcodeBoost/MFClangHelper.h
@@ -0,0 +1,15 @@
+//
+//  MFClangHelper.h
+//  XcodeBoost
+//
+//  Created by Michaël Fortin on 2014-04-06.
+//  Copyright (c) 2014 Michaël Fortin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFClangHelper : NSObject
+
+- (NSArray *)methodDefinitionRanges;
+
+@end
---
+++ XcodeBoost/MFClangHelper.m
@@ -0,0 +1,19 @@
+//
+//  MFClangHelper.m
+//  XcodeBoost
+//
+//  Created by Michaël Fortin on 2014-04-06.
+//  Copyright (c) 2014 Michaël Fortin. All rights reserved.
+//
+
+#import "MFClangHelper.h"
+#import "Index.h"
+
+@implementation MFClangHelper
+
+- (NSArray *)methodDefinitionRanges
+{
+	return nil;
+}
+
+@end
