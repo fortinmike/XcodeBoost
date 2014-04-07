b2700ec3b0fdb21a3f932589f6595fa7e31e0136
Michaël Fortin <fortinmike@irradiated.net>
2014-04-07 17:28:14 -0400

    Updated class-dump script.

--- Scripts/generate-class-dumps.sh
+++ Scripts/generate-class-dumps.sh
@@ -1,9 +1,10 @@
 # Created: 2014-03-19
 
 script_path=$(cd $(dirname $0) ; pwd -P)
+output_path="$script_path/../Headers"
 sdk="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
 
-class-dump -r /Applications/Xcode.app/Contents/Frameworks/IDEFoundation.framework > "$script_path/../Class-Dumps/IDEFoundation.h"
-class-dump -r /Applications/Xcode.app/Contents/Frameworks/IDEKit.framework > "$script_path/../Class-Dumps/IDEKit.h"
-class-dump -r /Applications/Xcode.app/Contents/SharedFrameworks/DVTFoundation.framework > "$script_path/../Class-Dumps/DVTFoundation.h"
-class-dump -r /Applications/Xcode.app/Contents/SharedFrameworks/DVTKit.framework > "$script_path/../Class-Dumps/DVTKit.h"
+class-dump -r /Applications/Xcode.app/Contents/Frameworks/IDEFoundation.framework > "$output_path/IDEFoundation.h"
+class-dump -r /Applications/Xcode.app/Contents/Frameworks/IDEKit.framework > "$output_path/IDEKit.h"
+class-dump -r /Applications/Xcode.app/Contents/SharedFrameworks/DVTFoundation.framework > "$output_path/DVTFoundation.h"
+class-dump -r /Applications/Xcode.app/Contents/SharedFrameworks/DVTKit.framework > "$output_path/DVTKit.h"
