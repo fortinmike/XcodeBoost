1673ebc6bef075e829dd4e7284f5dfebcda11cf2
Michaël Fortin <fortinmike@irradiated.net>
2014-05-19 11:58:28 -0400

    Simplified the method pattern a great deal to make it easier to maintain. It's less close to the signature spec but more forgiving.

--- XcodeBoost/NSString+XcodeBoost.m
+++ XcodeBoost/NSString+XcodeBoost.m
@@ -39,8 +39,8 @@ static NSString *s_commentPattern = @"//.+?(?=\\n)|/\\*.+?\\*/";
 
 #pragma mark Complex Patterns
 
-// Unescaped: ([-\+] \([a-zA-Z0-9 \(\)_\*^]+\) ?([a-zA-Z0-9_\*]+(:\([a-zA-Z0-9 \(\)_\*^]+\)[a-zA-Z0-9_]+[ \n]?)?\s*)+)\{(.*\n)+?(\n?)\}
-static NSString *s_methodPattern = @"([-\\+] \\([a-zA-Z0-9 \\(\\)_\\*^]+\\) ?([a-zA-Z0-9_\\*]+(:\\([a-zA-Z0-9 \\(\\)_\\*^]+\\)[a-zA-Z0-9_]+[ \\n]?)?\\s*)+)\\{(.*\\n)+?(\\n?)\\}";
+// Unescaped: ([-\+] ?[a-zA-Z0-9 \(\)_\*^:\n\s]+)\{(.*\n)+?(\n?)\}
+static NSString *s_methodPattern = @"([-\\+] ?[a-zA-Z0-9 \\(\\)_\\*^:\\n\\s]+)\\{(.*\\n)+?(\\n?)\\}";
 
 // Unescaped: ([a-zA-Z0-9_]+? [a-zA-Z0-9_]+?\(.+?\))\n?\{(.*\n)+?(\n?)\}
 static NSString *s_functionPattern = @"([a-zA-Z0-9_]+? [a-zA-Z0-9_]+?\\(.+?\\))\\n?\\{(.*\\n)+?(\\n?)\\}";
