fadd36b4963d58c3b9a66a3042d5b6386d0dcfae
Michaël Fortin <fortinmike@irradiated.net>
2014-05-25 20:52:29 -0400

    Added empty view controller for preferences window.

---
+++ XcodeBoost/MFPreferencesWindowController.h
@@ -0,0 +1,13 @@
+//
+//  MFPreferencesWindowController.h
+//  XcodeBoost
+//
+//  Created by Michaël Fortin on 2014-05-25.
+//  Copyright (c) 2014 Michaël Fortin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MFPreferencesWindowController : NSWindowController
+
+@end
---
+++ XcodeBoost/MFPreferencesWindowController.m
@@ -0,0 +1,27 @@
+//
+//  MFPreferencesWindowController.m
+//  XcodeBoost
+//
+//  Created by Michaël Fortin on 2014-05-25.
+//  Copyright (c) 2014 Michaël Fortin. All rights reserved.
+//
+
+#import "MFPreferencesWindowController.h"
+
+@implementation MFPreferencesWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self)
+	{
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+}
+
+@end
