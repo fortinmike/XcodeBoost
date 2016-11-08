a4950e95077da9676313d9213c833a5a59923086
gexiao <gexiao1994@gmail.com>
2016-11-08 21:44:11 +0800

    Update regex for method
    Add missing comma;
    Adapt generic syntax.

--- XcodeBoost/NSString+XcodeBoost.m
+++ XcodeBoost/NSString+XcodeBoost.m
@@ -46,8 +46,8 @@ static NSString *s_commentPattern = @"//.+?(?=\\n)|/\\*.+?\\*/";
 
 #pragma mark Complex Patterns
 
-// Unescaped: ([-\+] ?[a-zA-Z0-9 \(\)_,\*^:\n\s]+)\{(?:.*\n)+?(?:\n?)\}
-static NSString *s_methodPattern = @"([-\\+] ?[a-zA-Z0-9 \\(\\)_\\*^:\\n\\s]+)\\{(?:.*\\n)+?(?:\\n?)\\}";
+// Unescaped: ([-\+] ?[a-zA-Z0-9 \(\)_,\*^:\n\s\<\>]+)\{(?:.*\n)+?(?:\n?)\}
+static NSString *s_methodPattern = @"([-\\+] ?[a-zA-Z0-9 \\(\\)_,\\*^:\\n\\s\\<\\>]+)\\{(?:.*\\n)+?(?:\\n?)\\}";
 
 // Unescaped: ([a-zA-Z0-9\(\)_,\*^: ]+?[a-zA-Z0-9_]+?\(.*?\))[\s\n]*?\{(?:.*\n)+?(?:\n?)\}
 static NSString *s_functionPattern = @"([a-zA-Z0-9\\(\\)_,\\*^: ]+?[a-zA-Z0-9_]+?\\(.*?\\))[\\s\\n]*?\\{(?:.*\\n)+?(?:\\n?)\\}";
